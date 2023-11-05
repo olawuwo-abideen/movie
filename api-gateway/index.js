001b14bfbae11f7e2b7cb3a0d987084b1522d89d
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-05 21:41:05 +0100

    updated

---
+++ api-gateway/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/movie', proxy('http://localhost:3001'))
+app.use('/actor', proxy('http://localhost:3002'))
+app.use('/admin', proxy('http://localhost:3003'))
+app.use('/payment', proxy('http://localhost:3004'))
+app.use('/recommendation', proxy('http://localhost:3005'))
+app.use('/review', proxy('http://localhost:3006'))
+app.use('/user', proxy('http://localhost:3007'))
+app.use('/watchlist', proxy('http://localhost:3008'))
+
+app.listen(3000, () => {
+    console.log('Gateway is Listening to Port 3000')
+})
