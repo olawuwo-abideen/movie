a3635855ada0e7e61b52b609761f769d9c729c8b
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-07 14:55:07 +0100

    updated

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -6,6 +6,6 @@ const mongoose = require('mongoose');
 const app = express();
 
 
-app.listen(3000, () => {
-    console.log(`Actor Server running on port ${3004}`)
+app.listen(3002, () => {
+    console.log(`Actor Server running on port ${3002}`)
 })
