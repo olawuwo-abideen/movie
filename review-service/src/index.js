f0784fa99a09108887b930c52be77159412bc98d
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-11-07 15:13:57 +0100

    updated

--- review-service/src/index.js
+++ review-service/src/index.js
@@ -6,6 +6,6 @@ const mongoose = require('mongoose');
 const app = express();
 
 
-app.listen(3000, () => {
-    console.log(`Actor Server running on port ${3004}`)
+app.listen(3007, () => {
+    console.log(`Review Services running on port ${3007}`)
 })
