ef085e157f56d3e8af1b44ff588a69caa8edf524
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-02 22:27:42 +0100

    update services models

--- customer-service/src/index.js
+++ customer-service/src/index.js
@@ -1,11 +1,32 @@
-require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+const { PORT } = require('./config');
+const { databaseConnection } = require('./database');
+const expressApp = require('./express-app');
+const { CreateChannel } = require('./utils')
 
+const StartServer = async() => {
 
-const app = express();
+    const app = express();
+    
+    await databaseConnection();
 
+    const channel = await CreateChannel()
 
-app.listen(3002, () => {
-    console.log(`Customer Service running on port ${3002}`)
-})
+    await expressApp(app, channel);
+    
+
+    app.listen(PORT, () => {
+          console.log(`listening to port ${PORT}`);
+    })
+    .on('error', (err) => {
+        console.log(err);
+        process.exit();
+    })
+    .on('close', () => {
+        channel.close();
+    })
+    
+
+}
+
+StartServer();
