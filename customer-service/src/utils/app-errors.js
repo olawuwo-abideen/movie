b7175db71b984be59565a8a8d95e7cb702a135d4
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-02 22:03:42 +0100

    update customer services

---
+++ customer-service/src/utils/app-errors.js
@@ -0,0 +1,47 @@
+const { StatusCodes } = require('http-status-codes');
+
+
+
+
+class AppError extends Error {
+    constructor(name,StatusCodes,description, isOperational, errorStack, logingErrorResponse){
+        super(description);
+        Object.setPrototypeOf(this,new.target.prototype);
+        this.name = name;
+        this.StatusCodes = StatusCodes;
+        this.isOperational = isOperational
+        this.errorStack = errorStack;
+        this.logError = logingErrorResponse;
+        Error.captureStackTrace(this);
+    }
+}
+
+//api Specific Errors
+class APIError extends AppError {
+    constructor(name, StatusCodes = StatusCodes.INTERNAL_SERVER_ERROR, description ='Internal Server Error',isOperational = true,){
+        super(name,StatusCodes.BAD_REQUEST,description,isOperational);
+    }
+}
+
+//400
+class BadRequestError extends AppError {
+    constructor(description = 'Bad request',logingErrorResponse){
+        super('NOT FOUND', StatusCodes.BAD_REQUEST,description,true, false, logingErrorResponse);
+    }
+}
+
+//400
+class ValidationError extends AppError {
+    constructor(description = 'Validation Error', errorStack){
+        super('BAD REQUEST', StatusCodes.BAD_REQUEST,description,true, errorStack);
+    }
+}
+
+
+module.exports = {
+    AppError,
+    APIError,
+    BadRequestError,
+    ValidationError,
+    StatusCodes,
+}
