f51de0f367ac7a99e367dc6c6e491d770d11a16e
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-04 20:47:00 +0100

    update shopping service

--- shopping-service/src/api/app-events.js
+++ shopping-service/src/api/app-events.js
@@ -1,15 +1,22 @@
 const { StatusCodes } = require('http-status-codes');
+const ShoppingService = require("../services/shopping-service");
+
 module.exports = (app) => {
     
+    const service = new ShoppingService();
+
     app.use('/app-events',async (req,res,next) => {
 
         const { payload } = req.body;
-
         console.log("============= Shopping ================");
+        
         console.log(payload);
 
-        return res.status(StatusCodes.OK).json({ message: 'notified!'});
- 
+         //handle subscribe events
+         service.SubscribeEvents(payload);
+         
+       return res.status(StatusCodes.OK).json({message: 'notified!'});
+
     });
 
 }
