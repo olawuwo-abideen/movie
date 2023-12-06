c149e442f52709bbc4a798904c13b6e5c91fdb2a
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-06 15:16:49 +0100

    update payment services

---
+++ payment-service/src/api/app-events.js
@@ -0,0 +1,15 @@
+module.exports = (app) => {
+    
+
+    app.use('/app-events',async (req,res,next) => {
+
+        const { payload } = req.body;
+
+        console.log("============= Payment ================");
+        console.log(payload);
+
+        return res.status(200).json({ message: 'notified!'});
+ 
+    });
+
+}
