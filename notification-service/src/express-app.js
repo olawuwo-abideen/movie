3b9fd045ddba7274bf1c3d5462621324f6e3853a
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-07 18:23:40 +0100

    update notification services

---
+++ notification-service/src/express-app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors  = require('cors');
+const { customer, appEvents } = require('./api');
+const { CreateChannel, SubscribeMessage } = require('./utils')
+
+module.exports = async (app) => {
+
+    app.use(express.json());
+    app.use(cors());
+    app.use(express.static(__dirname + '/public'))
+
+
+
+    const channel = await CreateChannel()
+
+    
+    customer(app, channel);
+ 
+    
+}
