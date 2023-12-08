121f774c38c4ffcfec6f770ab1f9a3f00ba869f3
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-08 15:59:18 +0100

    updated

---
+++ payment-service/src/express-app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors  = require('cors');
+const { payment, appEvents } = require('./api');
+const { CreateChannel, SubscribeMessage } = require('./utils')
+
+module.exports = async (app) => {
+
+    app.use(express.json());
+    app.use(cors());
+
+
+
+    const channel = await CreateChannel()
+
+    
+    customer(app, channel);
+ 
+    
+}
