b1e384db0f49db38a0df69d3253caca5c2aa2d96
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-14 15:07:26 +0100

    update movie services

--- movies-service/src/api/movies.js
+++ movies-service/src/api/movies.js
@@ -1,16 +1,15 @@
 const { StatusCodes } = require('http-status-codes');
-const { CUSTOMER_SERVICE, SHOPPING_SERVICE } = require("../config");
+const { RPCObserver } = require("../utils");
 const MovieService = require("../services/movie-service");
-const {
-  PublishCustomerEvent,
-  PublishShoppingEvent, 
-  PublishMessage,
-} = require("../utils");
+
 const UserAuth = require("./middlewares/auth");
 
 module.exports = (app, channel) => {
+  
   const service = new MovieService();
-
+  
+  RPCObserver("MOVIE_RPC", service);
+  
   app.post("/movie/create", async (req, res, next) => {
     const { title, genre, releaseYear, director, plot, rating, reviews, unit, price , available } =
       req.body;
@@ -60,73 +59,8 @@ module.exports = (app, channel) => {
     return res.status(StatusCodes.OK).json(movies);
   });
 
-  app.put("/wishlist", UserAuth, async (req, res, next) => {
-    const { _id } = req.user;
-
-    const { data } = await service.GetMoviePayload(
-      _id,
-      { movieId: req.body._id },
-      "ADD_TO_WISHLIST"
-    );
-    PublishMessage(channel, CUSTOMER_SERVICE, JSON.stringify(data));
-
-    res.status(StatusCodes.OK).json(data.data.product);
-  });
-
-  app.delete("/wishlist/:id", UserAuth, async (req, res, next) => {
-    const { _id } = req.user;
-    const movieId = req.params.id;
-
-    const { data } = await service.GetMoviePayload(
-      _id,
-      { movieId },
-      "REMOVE_FROM_WISHLIST"
-    );
-    PublishMessage(channel, CUSTOMER_SERVICE, JSON.stringify(data));
-
-    res.status(StatusCodes.OK).json(data.data.movie);
-  });
-
-  app.put("/cart", UserAuth, async (req, res, next) => {
-    const { _id } = req.user;
-
-    const { data } = await service.GetMoviePayload(
-      _id,
-      { movieId: req.body._id, qty: req.body.qty },
-      "ADD_TO_CART"
-    );
-
-
-    PublishMessage(channel, CUSTOMER_SERVICE, JSON.stringify(data));
-    PublishMessage(channel, SHOPPING_SERVICE, JSON.stringify(data));
-
-    const response = { movie: data.data.movie, unit: data.data.qty };
-
-    res.status(StatusCodes.OK).json(response);
-  });
-
-  app.delete("/cart/:id", UserAuth, async (req, res, next) => {
-    const { _id } = req.user;
-    const movieId = req.params.id;
-
-    const { data } = await service.GetMoviePayload(
-      _id,
-      { movieId },
-      "REMOVE_FROM_CART"
-    );
-
-    PublishMessage(channel, CUSTOMER_SERVICE, JSON.stringify(data));
-    PublishMessage(channel, SHOPPING_SERVICE, JSON.stringify(data));
-
-    const response = { movie: data.data.movie, unit: data.data.qty };
-
-    res.status(StatusCodes.OK).json(response);
-  });
-
-  app.get("/movies", async (req, res, next) => {
-    //check validation
+   app.get("/movies", async (req, res, next) => {
     try {
-      const { _id } = req.user;
       const { data } = await service.GetMovies();
       return res.status(StatusCodes.OK).json(data);
     } catch (error) {
@@ -134,3 +68,4 @@ module.exports = (app, channel) => {
     }
   });
 };
+
