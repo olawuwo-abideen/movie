46a273c54747cee17a4dfc0c9cfb418541a5b119
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-14 14:42:07 +0100

    update movie services

--- movies-service/src/express-app.js
+++ movies-service/src/express-app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors  = require('cors');
 const { movies, appEvents } = require('./api');
-const { CreateChannel, SubscribeMessage } = require('./utils')
+const { CreateChannel} = require('./utils')
 
 module.exports = async (app) => {
 
     app.use(express.json());
     app.use(cors());
 
+      //api
+  appEvents(app);
 
 
     const channel = await CreateChannel()
