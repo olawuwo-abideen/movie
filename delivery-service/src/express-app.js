bcd541b9d3677c068648888a54bc9331190427e5
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-15 15:59:21 +0100

    update delivery services

---
+++ delivery-service/src/express-app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const { delivery } = require("./api");
+const { CreateChannel } = require("./utils");
+
+module.exports = async (app) => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(express.static(__dirname + "/public"));
+  const channel = await CreateChannel();
+
+  customer(app, channel);
+};
