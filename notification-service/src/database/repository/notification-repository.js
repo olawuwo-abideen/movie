1f23fae73548e5aa57cb2500aaa2b63e56e3d722
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-12-19 23:40:58 +0100

    update payment services

---
+++ notification-service/src/database/repository/notification-repository.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { NotificationModel } = require("../models");
+
+//Dealing with data base operations
+class NotificationRepository {
+
+
+    
+    
+}
+
+module.exports = NotificationRepository;
