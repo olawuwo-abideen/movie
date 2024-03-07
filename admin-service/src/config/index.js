460c5e62c03eb27afe8cb5c2e96d0ea94eaccaf4
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-03-07 21:39:04 +0100

    updated

--- admin-service/src/config/index.js
+++ admin-service/src/config/index.js
@@ -13,6 +13,5 @@ module.exports = {
   APP_SECRET: process.env.APP_SECRET,
   EXCHANGE_NAME: process.env.EXCHANGE_NAME,
   MSG_QUEUE_URL: process.env.MSG_QUEUE_URL,
-  CUSTOMER_SERVICE: "customer_service",
-  SHOPPING_SERVICE: "shopping_service",
+  MOVIES_SERVICE: "movies_service",
 };
